4b6a1cb4a2d466eccba50968baa891ceb456044a
Franklinpollo123 <frsalasmo@est.utn.cr>
2025-07-03 14:28:22 -0600

    proyectoInicial

---
+++ src/proyectoprogra1/joss/fran/Proyectoprogra1JossFran.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectoprogra1.joss.fran;
+
+/**
+ *
+ * @author frank
+ */
+public class Proyectoprogra1JossFran {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
