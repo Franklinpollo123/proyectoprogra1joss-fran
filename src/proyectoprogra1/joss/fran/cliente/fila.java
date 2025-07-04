8f0059fa7e0ed7650676d7a0d942b216c92f9a0d
Franklinpollo123 <frsalasmo@est.utn.cr>
2025-07-03 20:16:32 -0600

    aqui  usamos la funcion de la prioridad se llama Queue es importante para ordenar filas

--- src/proyectoprogra1/joss/fran/cliente/fila.java
+++ src/proyectoprogra1/joss/fran/cliente/fila.java
@@ -14,6 +14,7 @@ import java.util.Queue;
  *
  * @author frank y joss
  */
+
 public class fila {
     private final int LIMITE_MAX = 25;
     private final Queue<cliente> colaA = new LinkedList<>();
